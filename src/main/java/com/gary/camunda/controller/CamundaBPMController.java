46961f3fd7b6a010e30040d7116634c60c82260a
garyma <garymar2007@gmail.com>
2025-09-21 21:22:24 +0200

    Finished hello world demo for SpringBoot + Camunda.

---
+++ src/main/java/com/gary/camunda/controller/CamundaBPMController.java
@@ -0,0 +1,19 @@
+package com.gary.camunda.controller;
+
+import org.camunda.bpm.engine.ProcessEngine;
+import org.camunda.bpm.engine.ProcessEngines;
+import org.camunda.bpm.engine.runtime.ProcessInstantiationBuilder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CamundaBPMController {
+
+    @GetMapping("/executetask")
+    public String execute() {
+        ProcessEngine processEngine = ProcessEngines.getDefaultProcessEngine();
+        ProcessInstantiationBuilder instance = processEngine.getRuntimeService().createProcessInstanceByKey("hello-id");//"first-javainuse");
+        instance.executeWithVariablesInReturn();
+        return "Execute Camunda BPMN Task Successfully";
+    }
+}
